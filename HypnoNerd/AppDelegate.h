fd02d6d1901a694264c944ba3c79ca8fa86ca9ab
Michael <michael.r808@gmail.com>
2015-03-07 11:09:02 -0600

    Initial Commit

---
+++ HypnoNerd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnoNerd
+//
+//  Created by mike on 3/7/15.
+//  Copyright (c) 2015 X-tic Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
