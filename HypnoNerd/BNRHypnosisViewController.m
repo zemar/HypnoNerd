2493c98909259cfd3f63bcabc94659273d5ed16d
Michael <michael.r808@gmail.com>
2015-03-11 22:17:56 -0500

    commit

--- HypnoNerd/BNRHypnosisViewController.m
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -9,6 +9,10 @@
 #import "BNRHypnosisViewController.h"
 #import "BNRHypnosisView.h"
 
+@interface BNRHypnosisViewController () <UITextViewDelegate>
+@end
+
+
 @implementation BNRHypnosisViewController
 
 - (void)loadView
@@ -17,6 +21,18 @@
     CGRect frame = [UIScreen mainScreen].bounds;
     
     BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] initWithFrame:frame];
+    
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+                              
+    // Setting the border style on the text field will allow us to see it more easily
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"Hypnotize me";
+    textField.returnKeyType = UIReturnKeyDone;
+    textField.autocapitalizationType = UITextAutocapitalizationTypeSentences;
+    textField.delegate = self;
+    
+    [backgroundView addSubview:textField];
     
     // Set it as the view of the view controller
     self.view = backgroundView;
@@ -46,6 +62,12 @@
     // Always call the super implementation of viewDidLoad
     [super viewDidLoad];
     NSLog(@"BNRHypnosisViewController loaded its view.");
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    NSLog(@"%@", textField.text);
+    return YES;
 }
 
 @end
