63e412882b20cde05da22e1fe322821e7f7890a7
Michael <michael.r808@gmail.com>
2015-03-12 21:33:11 -0500

    commit

--- HypnoNerd/AppDelegate.h
+++ HypnoNerd/AppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UIScrollViewDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
