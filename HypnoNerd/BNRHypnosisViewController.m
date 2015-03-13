2b4ba5256cbce8ffd8aebd1773caf2a1acfc008d
Michael <michael.r808@gmail.com>
2015-03-12 19:28:25 -0500

    commit

--- HypnoNerd/BNRHypnosisViewController.m
+++ HypnoNerd/BNRHypnosisViewController.m
@@ -66,8 +66,44 @@
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    NSLog(@"%@", textField.text);
+    [self drawHypnoticMessage:textField.text];
+    
+    textField.text = @"";
+    [textField resignFirstResponder];
+    
     return YES;
+}
+
+- (void)drawHypnoticMessage:(NSString *)message
+{
+    for (int i = 0; i < 20; i++) {
+        UILabel *messageLabel = [[UILabel alloc] init];
+        
+        // Configure the label's colors and text
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor blackColor];
+        messageLabel.text = message;
+        
+        // This method resizes the label, which will be relative
+        // to the text that it is displaying
+        [messageLabel sizeToFit];
+        
+        // Get a random x value that fits within the hypnosis view's width
+        int width = self.view.bounds.size.width - messageLabel.bounds.size.width;
+        int x = arc4random() % width;
+        
+        // Get a random y value that fits within the hypnosis view's height
+        int height = self.view.bounds.size.height - messageLabel.bounds.size.height;
+        int y = arc4random() % height;
+        
+        // Update the label's frame
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+        
+        // Add the label to the hierarchy
+        [self.view addSubview:messageLabel];
+    }
 }
 
 @end
